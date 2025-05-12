dd4346e182c3965e168c8af9f8f11f2a75e8ad02
Dwigth Astacio <dwigth32@outlook.es>
2025-05-12 12:19:13 -0600

    Updated blueprint.ts

---
+++ blueprint.ts
@@ -0,0 +1,26 @@
+export const blueprint = {
+  "name": "Contacts",
+  "slug": "contacts",
+  "fields": [
+    {
+      "key": "firstName",
+      "type": "string",
+      "label": "First Name"
+    },
+    {
+      "key": "lastName",
+      "type": "string",
+      "label": "Last Name"
+    },
+    {
+      "key": "email",
+      "type": "string",
+      "label": "Email"
+    },
+    {
+      "key": "favorite-color",
+      "type": "string",
+      "label": "Favorite Color"
+    }
+  ]
+}
